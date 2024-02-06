d072bbf481b859332576c183f346501ac09b8928
Sai Aryan Goswami <saiaryan.goswami1784@gmail.com>
2024-02-06 14:59:18 +0530

    Team.T.B.D

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import './App.css'
-import Navbar from './components/Navbar'
-import Home from './components/Home'
-import AboutUs from './components/AboutUs'
-import Contact from './components/Contact'
-import Services from './components/Services'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+// import Navbar from './components/Navbar'
+// import Home from './components/Home'
+// import AboutUs from './components/AboutUs'
+// import Contact from './components/Contact'
+// import Services from './components/Services'
+// import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import TeachableMachineComponent from './components/TeachableMachineComponent'
 
 
@@ -13,17 +13,18 @@ function App() {
   return (
     <>
       <div>
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Navbar/>}>
-              <Route index element={<Home/>}/>
-              <Route path='/AboutUs' element={<AboutUs/>}/>
-              <Route path='/Services' element={<Services/>}/>
-              <Route path='/Contact' element={<Contact/>}/>
-              <Route path='/tmcomp' element={<TeachableMachineComponent/>}/>
-            </Route>
+        <TeachableMachineComponent/>
+        {/* <BrowserRouter> */}
+          {/* <Routes> */}
+            {/* <Route path="/" element={<Navbar/>}> */}
+              {/* <Route index element={<Home/>}/> */}
+              {/* <Route path='/AboutUs' element={<AboutUs/>}/> */}
+              {/* <Route path='/Services' element={<Services/>}/> */}
+              {/* <Route path='/Contact' element={<Contact/>}/> */}
+              {/* <Route path='/tmcomp' element={<TeachableMachineComponent/>}/> */}
+            {/* </Route>
           </Routes>
-        </BrowserRouter>
+        </BrowserRouter> */}
       </div>
     </>
   )
--- src/components/TeachableMachineComponent.jsx
+++ src/components/TeachableMachineComponent.jsx
@@ -1,16 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import * as tmImage from '@teachablemachine/image';
-// import { Canvas } from "react-three-fiber";
-// import { useGLTF } from 'drei';
-// import penModelPath from '../assets/Black & Gray Gel Pen.glb';
-// import { Application } from '@splinetool/runtime';
 import Spline from '@splinetool/react-spline';
-
-
-
-// const canvas = document.getElementById('canvas3d');
-//     const app = new Application(canvas);
-//     app.load('https://prod.spline.design/R0RAGT2Y9eZzPf4m/scene.splinecode');
 
 const TeachableMachineComponent = () => {
   const URL = 'https://teachablemachine.withgoogle.com/models/Tc8AGqgPM/';
@@ -19,16 +9,13 @@ const TeachableMachineComponent = () => {
   const [labelContainer, setLabelContainer] = useState(null);
   const [maxPredictions, setMaxPredictions] = useState(null);
   const [predictions, setPredictions] = useState([]);
-
-
-    
-
+  const [incrementPoints, setIncrementPoints] = useState(0);
+  const [splineUrl, setSplineUrl] = useState('https://prod.spline.design/R0RAGT2Y9eZzPf4m/scene.splinecode'); // Initial Spline URL
 
   useEffect(() => {
     const init = async () => {
       const modelURL = URL + 'model.json';
       const metadataURL = URL + 'metadata.json';
-      
 
       // load the model and metadata
       const loadedModel = await tmImage.load(modelURL, metadataURL);
@@ -37,7 +24,7 @@ const TeachableMachineComponent = () => {
 
       // Convenience function to set up a webcam
       const flip = true; // whether to flip the webcam
-      const webcamInstance = new tmImage.Webcam(500, 300, flip); // width, height, flip
+      const webcamInstance = new tmImage.Webcam(600, 300, flip); // width, height, flip
       await webcamInstance.setup(); // request access to the webcam
       await webcamInstance.play();
       setWebcam(webcamInstance);
@@ -84,6 +71,15 @@ const TeachableMachineComponent = () => {
     window.requestAnimationFrame(loop);
   }, [webcam]);
 
+  useEffect(() => {
+    // Update spline URL based on points
+    if (incrementPoints === 0) {
+      setSplineUrl('https://prod.spline.design/PAEO9zunTz1ufVw0/scene.splinecode'); // Eraser URL
+    } else if (incrementPoints === 1) {
+      setSplineUrl('https://prod.spline.design/R0RAGT2Y9eZzPf4m/scene.splinecode'); // Pen URL
+    }
+  }, [incrementPoints]);
+
   const predict = async () => {
     if (model && webcam) {
       // predict can take in an image, video, or canvas html element
@@ -94,66 +90,45 @@ const TeachableMachineComponent = () => {
         labelContainer.childNodes[i].innerHTML = `${prediction[i].className}: ${(
           prediction[i].probability * 100
         ).toFixed(2)}%`;
-        const objecthtml = document.getElementById('object');
-        if((prediction[i].probability * 100)>66)
-            objecthtml.innerHTML = `${prediction[i].className}`;
+
+        const incrementDiv = document.getElementById('points');
+        const congratsText = document.getElementById('congrats');
+        incrementDiv.innerHTML = `Points: ${incrementPoints}`;
+        const clasSes = document.getElementById('Classes')
+
+        if ((prediction[i].probability * 100) > 85) {
+          clasSes.innerHTML = `${prediction[i].className}`
+          // Check prediction based on current spline URL
+          if (splineUrl === 'https://prod.spline.design/PAEO9zunTz1ufVw0/scene.splinecode' && prediction[i].className === "Eraser") {
+            setIncrementPoints(incrementPoints + 1); // Increment points if eraser is detected
+            congratsText.innerHTML = "Good Job Kiddo! Now moving on to the next thing ";
+            incrementDiv.innerHTML = `Points: ${incrementPoints}`;
+          } else if (splineUrl === 'https://prod.spline.design/R0RAGT2Y9eZzPf4m/scene.splinecode' && prediction[i].className === "Pen") {
+            setIncrementPoints(incrementPoints + 1); // Increment points if pen is detected
+            congratsText.innerHTML = "Congratulations! You've completed the task!";
+            incrementDiv.innerHTML = `Points: ${incrementPoints}`;
+          }
+        }
       }
     }
   };
-  
-  // const PenModel = () => {
-  //   const { nodes, materials } = useGLTF(penModelPath);
-  //   return <primitive object={nodes['Pen']} />;
-  // };
 
   return (
     <div>
-      <Spline
-  scene="https://prod.spline.design/R0RAGT2Y9eZzPf4m/scene.splinecode"
-  style={{
-    width: '80%',
-    height: '50vh',
-    margin: 'auto',  // Center-align horizontally
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-    marginTop: '20px',  // Add top margin
-  }}
-/>
-      <h2 id='object' className='h-auto m-[5rem] flex items-center justify-center'></h2>
-      <div
-        id="webcam-container"
-        className="h-auto flex items-center justify-center mb-[3rem]"
-      >
+      <h1 id='congrats' className='h-auto m-[1rem] flex items-center justify-center bg-gradient-to-r from-blue-600 via-green-500 to-indigo-400 text-transparent bg-clip-text text-4xl font-bold'></h1>
+      <h1 id='points' className='h-auto m-[1rem] text-right mr-[1rem]'></h1>
+      <h1 id='Classes'></h1>
+      
+      <div className='h-auto flex items-center justify-center mt-[3rem] mb-[2rem]'>
+      <Spline className='shadow-2xl' scene={splineUrl} style={{ width: '50%', height: '50vh', margin: 'auto', display: 'flex', alignItems: 'center', justifyContent: 'center', marginTop: '20px' }} />
+        <div
+          id="webcam-container"
+          className='shadow-2xl'
+        >
+        </div>
       </div>
-      <div id="label-container" className=' hidden'></div>
-
+      <div id="label-container" className='hidden'></div>
       <p id='canvas3d'></p>
-
-      {/* {predictions.map((prediction, index) => (
-        <div key={index} className="flex justify-between mb-1">
-          <span className="text-base font-medium text-blue-700 ">
-            {prediction.className}
-          </span>
-          <span className="text-sm font-medium text-blue-700">
-            {(prediction.probability * 100).toFixed(2)}%
-          </span>
-        </div>
-      ))}
-
-      {predictions.length > 0 && (
-        <div className="w-[40rem] rounded-full h-[2rem] m-[2rem]">
-          {predictions.map((prediction, index) => (
-            <div
-              key={index}
-              className="bg-blue-600 h-[2rem] rounded-full"
-              style={{
-                width: `calc(${(prediction.probability * 100).toFixed(2)}%)`,
-              }}
-            ></div>
-          ))}
-        </div>
-      )} */}
     </div>
   );
 };
